9c948fd9d42227aaa42ccfdda1f00fff8f5119d3
Sigit Purnomo <sigit@staff.uajy.ac.id>
2018-09-06 18:21:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,30 +8,30 @@ bahasaID = Flask(__name__)
 Swagger(bahasaID)
 
 def createJSONSentenceList(listName):
-    default_index = '0'
+    #default_index = '0'
     default_item ='blank'
     hasilKalimat = {}
     hasilKalimat['daftarKalimat'] = [] 
+    hasilKalimat = adict.get('daftarKalimat',default_item)
     list_kalimat = {}
-    hasilKalimat['daftarKalimat'] = adict.get('no_kalimat',default_index)
-    hasilKalimat['daftarKalimat'] = adict.get('teks_kalimat',default_item)
-    for index, item in enumerate(listName):
-        
+    #hasilKalimat['daftarKalimat'] = adict.get('no_kalimat',default_index)
+    #hasilKalimat['daftarKalimat'] = adict.get('teks_kalimat',default_item)
+    for index, item in enumerate(listName)   
         list_kalimat['no_kalimat'] = index
         list_kalimat['teks_kalimat'] = item
         hasilKalimat['daftarKalimat'].append(list_kalimat)
     return hasilKalimat
 
 def createJSONWordsList(listName):
-    default_index = '0'
+    #default_index = '0'
     default_item ='blank'
     hasilKata = {}
-    hasilKata['daftarKata'] = []    
-    hasilKata['daftarKata'] = adict.get('no_kata', default_index)
-    hasilKata['daftarKata'] = adict.get('teks_kata', default_item)
+    hasilKata['daftarKata'] = []   
+    hasilKata = adict.get('daftarKata',default_item)
+    #hasilKata['daftarKata'] = adict.get('no_kata', default_index)
+    #hasilKata['daftarKata'] = adict.get('teks_kata', default_item)
     list_kata = {}
     for index, item in enumerate(listName):
-        
         list_kata['no_kata'] = index
         list_kata['teks_kata'] = item
         hasilKata['daftarKata'].append(list_kata)
